367fee7c866f8adfa6f31d91230cfb0f9c697a80
Naveen <sheoran19997@gmail.com>
2023-08-19 21:55:56 +0530

    pushing through terminal

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const number = document.getElementById('number').value;
+    const date = document.getElementById('date').value;
+    const time = document.getElementById('time').value;
+  
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Number:',number);
+    console.log('Number:',date);
+    console.log('Number:',time);
+  });
