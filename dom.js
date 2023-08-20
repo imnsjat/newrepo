d1ef412f621967e264d1c8d3d2790f6f97ea16c4
Naveen <sheoran19997@gmail.com>
2023-08-20 08:51:21 +0530

    upto 23 min video 1

---
+++ dom.js
@@ -0,0 +1,22 @@
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+
+// const headertitle = document.getElementById('header-title');
+// console.log(headertitle);
+// headertitle.textContent = 'hello';
+// headertitle.innerText = 'hello2'  // pays attenion to sytling wont show if hidden
+// headertitle.innerHTML='<h3>Hello</h3>';
+// headertitle.style.borderBottom = 'solid 3px #000';
+
+const header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+const additems = document.getElementById('add-items');
+additems.style.color = ' green ';
+additems.style.fontWeight = 'bold';
+ 
