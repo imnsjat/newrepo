27cf6ee4945f063fcc7cda523f1965d41d2b8c9d
Naveen <sheoran19997@gmail.com>
2023-08-20 09:04:19 +0530

    upto 27 min

--- dom.js
+++ dom.js
@@ -20,3 +20,11 @@ const additems = document.getElementById('add-items');
 additems.style.color = ' green ';
 additems.style.fontWeight = 'bold';
  
+
+const items = document.getElementsByClassName('list-group-item');
+//task : Make the 3 rd element in the list have green background color
+items[2].style.background = 'green' ;
+ // Make all the elements in the list have bold color font
+ for(let i=0;i<items.length ; i++){
+    items[i].style.fontWeight = 'bold';
+ }
